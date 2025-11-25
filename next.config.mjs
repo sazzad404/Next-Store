8cfcb87553153c3c04ec250af1e6255cc0f32786
Md Sazzad Hasan <sazzadhasan313@gmail.com>
2025-11-25 16:56:37 +0600

    add readme

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",                    // এটা সবচেয়ে জরুরি (static export)
-  trailingSlash: true,                 // Firebase Hosting-এর জন্য দরকার
+  // output: "export",                   
+  trailingSlash: true,                 
   images: {
-    unoptimized: true,                 // এটা না দিলে build fail করবে (Firebase-এ Image Optimization চলে না)
+    unoptimized: true,                
     domains: [
       "i.ibb.co.com",
       "cdn.rareblocks.xyz", 
       "i.pravatar.cc"
     ],
   },
-  reactCompiler: true,                 // চাইলে রাখতে পারো (Next.js 14+)
-  // distDir: "out",                   // ডিফল্টই out, চাইলে লিখতে পারো
+  reactCompiler: true,                
+  // distDir: "out",                   
 };
 
-export default nextConfig; // default এর বদলে এভাবে export করো (Next.js 13+ এর সাথে কম্প্যাটিবল)
+export default nextConfig; 
